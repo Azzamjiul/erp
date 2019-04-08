77846304d689220234328ac83fc9820490f895dc
Azzam Jihad Ulhaq <azzamjiul@MacBook-Pro-Azzam.local>
2019-04-08 14:16:25 +0700

    nambahin hoho

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel hoho
                 </div>
 
                 <div class="links">
