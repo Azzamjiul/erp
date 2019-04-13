0ab8e8357765b8280c2e60a818a75b8ae2fe702d
azzam <azzam.jiul@gmail.com>
2019-04-14 00:17:37 +0700

    update account index view

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -8,146 +8,43 @@
 <h1>Chart of Account</h1>
 @stop
 
-@section('')
-<div class="row">
-    <div class="col-xs-12">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <div class="input-group-btn">
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal-default">
-                        Tambah Kode Akun
-                    </button>
-                </div>
-                <div class="box-tools">
-                    <div class="input-group input-group-sm">
-                        <input type="text" class="form-control pull-right" name="table_search" placeholder="search">
-                        <div class="input-group-btn">
-                            <button class="btn btn-default" type="submit">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.box-header -->
-
-            <div class="box-body">
-                <ul class="file-tree">
-                    <li><a href="#">Asset</a>
-                        <ul>
-                            <li><a href="#">Asset Lancar</a>
-                                <ul>
-                                    <li><a href="#link2">Link 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Asset Tetap</a>
-                                <ul>
-                                    <li><a href="#">Link 2</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Kewajiban</a>
-                        <ul>
-                            <li><a href="#">Link 1</a> </li>
-                            <li><a href="#">Link 2</a> </li>
-                            <li><a href="#">Link 3</a> </li>
-                            <li><a href="#">Link 4</a> </li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Ekuitas</a>
-                        <ul>
-                            <li><a href="#">Link 1</a> </li>
-                            <li><a href="#">Link 2</a> </li>
-                            <li><a href="#">Link 3</a> </li>
-                            <li><a href="#">Link 4</a> </li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Pendapatan</a>
-                        <ul>
-                            <li><a href="#">Link 1</a> </li>
-                            <li><a href="#">Link 2</a> </li>
-                            <li><a href="#">Link 3</a> </li>
-                            <li><a href="#">Link 4</a> </li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Beban</a>
-                        <ul>
-                            <li><a href="#">Link 1</a> </li>
-                            <li><a href="#">Link 2</a> </li>
-                            <li><a href="#">Link 3</a> </li>
-                            <li><a href="#">Link 4</a> </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.box-body -->
-        </div>
-    </div>
-</div>
-<div class="modal fade" id="modal-default">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Form Menambah Kode Akun</h4>
-      </div>
-      <div class="modal-body">
-        <form role="form">
-          <!-- text input -->
-          <div class="form-group">
-            <label>Account Group</label>
-            <select name="" id="" class="form-control">
-              <option value="">1 - Asset</option>
-              <option value="">2 - Kewajiban</option>
-              <option value="">3 - Ekuitas</option>
-              <option value="">4 - Pendapatan</option>
-              <option value="">5 - Beban</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label>Account Parent</label>
-            <select name="" id="" class="form-control">
-              <option value="">1-20101 Tanah</option>
-              <option value="">1-20102 Bangunan</option>
-              <option value="">3 - Ekuitas</option>
-              <option value="">4 - Pendapatan</option>
-              <option value="">5 - Beban</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="text" value="1-20101">
-            <input type="text" class="">
-          </div>
-          <div class="form-group">
-            <label>Account Name</label>
-            <input type="text" class="form-control" placeholder="Masukkan Besar Modal">
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Tambah Kode Akun</button>
-      </div>
-    </div>
-    <!-- /.modal-content -->
-  </div>
-  <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
-@stop
 
 @section('content')
 <div class="container">
-    <table title="Account" class="easyui-treegrid" method="get" style="width:700px;height:300px" url="getAccount" rownumbers="true" idField="id" treeField="account_number">
+    <table title="Account" class="easyui-treegrid" method="get" style="width:100%;height:100%" url="getAccount" rownumbers="true" idField="id" treeField="account_number" toolbar="#toolbar">
         <thead>
             <tr>
                 <th field="account_number" width="300">Account Number</th>
-                <th field="account_name" width="100" align="right">Account Name</th>
+                <th field="account_name" width="300" align="left">Account Name</th>
             </tr>
         </thead>
     </table>
+    <div id="toolbar">
+        <a href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newAccount()">New Account</a>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editUser()">Edit User</a>
+        <a href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyUser()">Remove User</a>
+    </div>
+    <div id="dlg" class="easyui-dialog" style="width:400px;height:280px;padding:10px 20px" closed="true" buttons="#dlg-buttons">
+        <div class="ftitle">Tambah Kode Akun</div>
+        <form id="fm" method="post" novalidate>
+            <div class="fitem form-group">
+                <label>Account Parent   :</label>
+                <input name="firstname" class="easyui-textbox" required="true">
+            </div>
+            <div class="fitem form-group">
+                <label>Account Code     :</label>
+                <input name="lastname" class="easyui-textbox" required="true">
+            </div>
+            <div class="fitem form-group">
+                <label>Account Name     :</label>
+                <input name="phone" class="easyui-textbox">
+            </div>
+        </form>
+    </div>
+    <div id="dlg-buttons">
+        <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="saveAccount()" style="width:90px">Save</a>
+        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancel</a>
+    </div>
 </div>
 @stop
 
@@ -164,60 +61,43 @@
     console.log('Hi!');
 </script>
 
-// file-exploler tree
-<script src="//code.jquery.com/jquery.min.js"></script>
-<script src="js/file-explore.js"></script>
-<script>
-    $(document).ready(function() {
-        $(".file-tree").filetree();
-    });
-
-    $(document).ready(function() {
-        $(".file-tree").filetree({
-            animationSpeed: 'fast'
-        });
-    });
-
-    $(document).ready(function() {
-        $(".file-tree").filetree({
-            collapsed: true,
-        });
-    });
-</script>
-
 <!-- Easy UI JS -->
 <script type="text/javascript" src="{{asset('vendor/jeasyui/jquery.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('vendor/jeasyui/jquery.easyui.min.js')}}"></script>
-@stop
+<script type="text/javascript">
+    function newAccount(){
+        $('#dlg').dialog('open').dialog('setTitle','New Account');
+        $('#fm').form('clear');
+        url = 'save_user.php';
+    }
 
-@section('css')
-<link rel="stylesheet" href="/css/admin_custom.css">
-<link href="css/file-explore.css" rel="stylesheet">
-@stop
-
-@section('js')
-<script>
-    console.log('Hi!');
-</script>
-
-// file-exploler tree
-<script src="//code.jquery.com/jquery.min.js"></script>
-<script src="js/file-explore.js"></script>
-<script>
-    $(document).ready(function() {
-        $(".file-tree").filetree();
-    });
-
-    $(document).ready(function() {
-        $(".file-tree").filetree({
-            animationSpeed: 'fast'
+    function saveAccount(){
+        $('#fm').form('submit',{
+            url: url,
+            onSubmit: function(){
+                return $(this).form('validate');
+            },
+            success: function(result){
+                var result = eval('('+result+')');
+                if (result.errorMsg){
+                    $.messager.show({
+                        title: 'Error',
+                        msg: result.errorMsg
+                    });
+                } else {
+                    $('#dlg').dialog('close');        // close the dialog
+                    $('#dg').datagrid('reload');    // reload the user data
+                }
+            }
         });
-    });
+    }
 
-    $(document).ready(function() {
-        $(".file-tree").filetree({
-            collapsed: true,
-        });
-    });
+    var row = $('#dg').datagrid('getSelected');
+    if (row){
+        $('#dlg').dialog('open').dialog('setTitle','Edit User');
+        $('#fm').form('load',row);
+        url = 'update_user.php?id='+row.id;
+    }
 </script>
 @stop
+
